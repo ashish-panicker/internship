cc55941fffc5364389c0884cac48398df6494a1c
ashish-panicker <ashish.s.panicker@gmail.com>
2021-04-06 10:30:37 +0530

    collections sorting

--- Java/collections/CollectionsDemo.java
+++ Java/collections/CollectionsDemo.java
@@ -1,6 +1,9 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -9,16 +12,78 @@ import java.util.Set;
 
 public class CollectionsDemo {
 
-    public static void main(String[] args) {
+    class InternEmailComparator implements Comparator<Intern> {
 
-        CollectionsDemo collectionsDemo;
-
-        collectionsDemo = new CollectionsDemo();
-        collectionsDemo.createHashSet();
-        collectionsDemo.createArrayList();
+        @Override
+        public int compare(Intern o1, Intern o2) {
+            return o1.getEmail().compareTo(o2.getEmail());
+        }
     }
 
-    public void createHashSet(){
+    public static void main(String[] args) {
+
+        CollectionsDemo collectionsDemo = new CollectionsDemo();
+        collectionsDemo.internsArrayList();
+    }
+
+    public void internsArrayList() {
+        List<Intern> internsList = new ArrayList<>();
+
+        internsList.add(new Intern(900345, "Arun Kumar", "arun.kumar"));
+        internsList.add(new Intern(900129, "Rahul pramanik", "pramanik.rahul2"));
+        internsList.add(new Intern(895015, "Krishna", "krishna.01"));
+
+        System.out.println("\n Before sorting..");
+        for (Intern intern : internsList) {
+            System.out.println(intern);
+        }
+
+        System.out.println("\n After sorting using empID..");
+        Collections.sort(internsList);
+        for (Intern intern : internsList) {
+            System.out.println(intern);
+        }
+
+        System.out.println("\n After sorting using email..");
+        Collections.sort(internsList, new InternEmailComparator());
+        for (Intern intern : internsList) {
+            System.out.println(intern);
+        }
+
+        System.out.println("\n After sorting using name..");
+        Collections.sort(internsList, new Comparator<Intern>() {
+            @Override
+            public int compare(Intern o1, Intern o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        for (Intern intern : internsList) {
+            System.out.println(intern);
+        }
+    }
+
+    public void sortArrayList() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.addAll(Arrays.asList(9, 3, 1, 6, 7, 4, 0, 2, 3));
+
+        System.out.println("\nBefore sorting..");
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        Collections.sort(numbers);
+
+        System.out.println("\nfter sorting..");
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+    }
+
+    public void createHashSet() {
         Set<String> names = new HashSet<>();
 
         names.add("Ashish"); // 0
@@ -27,7 +92,7 @@ public class CollectionsDemo {
         names.add("Rahul"); // 1
 
         Iterator<String> iterator = names.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
     }
@@ -52,7 +117,7 @@ public class CollectionsDemo {
 
         // System.out.println("\n\n================ArrayList=================");
         // for (String name : names) {
-        //     System.out.println(name);
+        // System.out.println(name);
         // }
     }
 
@@ -69,3 +134,54 @@ public class CollectionsDemo {
 
 }
 
+class Intern implements Comparable<Intern> {
+
+    private Integer empId;
+    private String name;
+    private String email;
+
+    public Intern() {
+    }
+
+    public Intern(Integer empId, String name, String email) {
+        this.empId = empId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getEmpId() {
+        return this.empId;
+    }
+
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int compareTo(Intern o) {
+        return this.getEmpId().compareTo(o.getEmpId());
+    }
+
+    @Override
+    public String toString() {
+        return "{" + " empId='" + getEmpId() + "'" + ", name='" + getName() + "'" + ", email='" + getEmail() + "'"
+                + "}";
+    }
+
+}
